10f461af891f0fd6c4bb19d5ddb970b5a57246d5
Saleem Mirza <saleem-mirza@users.noreply.github.com>
2016-10-12 07:11:04 -0400

    Added license to file headers

--- src/Serilog.Sinks.AzureAnalytics/Extensions/JsonExtensions.cs
+++ src/Serilog.Sinks.AzureAnalytics/Extensions/JsonExtensions.cs
@@ -1,16 +1,28 @@
-﻿using System.Collections.Generic;
+﻿// Copyright 2016 Zethian Inc.
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System.Collections.Generic;
 using Newtonsoft.Json.Linq;
 
-namespace Serilog.Extensions
+namespace Serilog.Sinks.AzureAnalytics.Extensions
 {
     public static class JsonExtensions
     {
         public static IDictionary<string, object> Flaten(this JArray jsonArray)
         {
             if (jsonArray == null)
-            {
                 return null;
-            }
 
             var dict = new Dictionary<string, object>();
             FlatenJToken(dict, jsonArray, string.Empty);
--- src/Serilog.Sinks.AzureAnalytics/Sinks/AzureAnalytics/AzureAnalyticsSink.cs
+++ src/Serilog.Sinks.AzureAnalytics/Sinks/AzureAnalytics/AzureAnalyticsSink.cs
@@ -1,23 +1,37 @@
-﻿using System;
+﻿// Copyright 2016 Zethian Inc.
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
 using System.Collections.Generic;
 using System.Net;
 using System.Security.Cryptography;
+using System.Text;
+using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using Serilog.Core;
 using Serilog.Events;
-using Serilog.Sinks.Batch;
-using Serilog.Extensions;
-using Newtonsoft.Json;
-using System.Text;
+using Serilog.Sinks.AzureAnalytics.Batch;
+using Serilog.Sinks.AzureAnalytics.Extensions;
 
 namespace Serilog.Sinks.AzureAnalytics
 {
     internal class AzureAnalyticsSink : BatchProvider, ILogEventSink
     {
-        private readonly string _workSpaceId;
-        private readonly string _logName;
-        private readonly string _authenticationId;
         private readonly Uri _analyticsUrl;
+        private readonly string _authenticationId;
+        private readonly string _logName;
+        private readonly string _workSpaceId;
 
         internal AzureAnalyticsSink(
             string workSpaceId,
